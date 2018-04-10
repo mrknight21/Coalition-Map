b7ab366f4447987515620c2fcf0efb73b4ff6451
jwwnz <jameswon.nz@gmail.com>
2018-04-10 16:43:57 +1200

    Linking Firebase database to mapPage
    Setting up state for mapPage.js
    
    Setting up lifecycle methods componentDidMount.
    
    Link Firebase database to mapPage by:
    
    1. accessing Database entries and storing database entries details to
    State.
    
    2. accessing navigator of user device and getting geolocation with
    results being updated to Firebase database

--- components/mapPage.js
+++ components/mapPage.js
@@ -1,8 +1,14 @@
 import React from 'react';
-import { Text, View } from 'react-native';
-import { Card } from 'react-native-elements';
+import {Text, View} from 'react-native';
+import {Card} from 'react-native-elements';
+import database from './firebase/firebase';
 
 class mapPage extends React.Component {
+
+    state = {
+        participants: []
+    };
+
     static navigationOptions = {
         title: 'Map',
         headerStyle: {
@@ -14,11 +20,53 @@ class mapPage extends React.Component {
         },
     };
 
+    // ---------------- Component lifecycle methods -----------------------------
+    componentDidMount() {
+
+        try {
+            this.interval = setInterval(() => { navigator.geolocation.getCurrentPosition(showPosition) }, 5000);
+            const showPosition = (position) => {
+                console.log("Fired!");
+                console.log(position.coords.latitude)
+                console.log(position.coords.longitude)
+                database.ref('testMapA/testUser1').update({
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude
+                });
+            }
+
+            console.log("hello");
+            database.ref('testMapA')
+                .on('value', (snapshot) => {
+                    const participants = [];
+                    snapshot.forEach((childSnapshot) => {
+                        participants.push({
+                            id: childSnapshot.key,
+                            userName: childSnapshot.child("userName").val().toString(),
+                            lat: childSnapshot.child('lat').val().toString(),
+                            lng: childSnapshot.child('lng').val().toString()
+                            // ...childSnapshot.val()
+                        });
+                    })
+                    console.log('parti2:');
+                    console.log(participants);
+                    this.setState(() => ({ participants: participants }));
+                }, (error) => {
+                    console.log("Error", error);
+                });
+
+        } catch (e) {
+            console.log("error", e);
+        }
+    }
+
+
+    //--------------------------- rendering method ---------------------------
     render() {
         return (
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
                 <Card>
-                <Text>Map</Text>
+                    <Text>Map</Text>
                 </Card>
             </View>
         );
