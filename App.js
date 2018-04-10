1ab20b843fef803351147ee3d3cb3f25e0d04c68
mche618 <mche618@aucklanduni.ac.nz>
2018-04-10 12:14:40 +1200

    Add dependency of react-native-elements
    react-native-elements added and tested with "Card"
    components

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {Card}from 'react-native-elements';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
+        <Card>
         <Text>Open up App.js to start working on your app!</Text>
+        </Card>
       </View>
     );
   }
